c83cce98a3a2b60254831c87f9ea7797993c7886
Joonho Moon <lc24-0360@lclark.edu>
2026-02-13 13:07:58 -0800

    Initial commit

---
+++ expbo_sim.py
@@ -0,0 +1,16 @@
+# Global Variables
+max_k = 14 # max senders
+max_b = 4 # max backoff
+sample_size = 1000 # number of trials
+
+def ev_oneOK(k, xb): # estimated value for time to send 1st packet
+    return None
+
+def run_trail(k, xb): # time taken on single attempt to send 1st packet
+    return None
+
+def main():
+    print()
+
+if __name__ == '__main__':
+    main()  
